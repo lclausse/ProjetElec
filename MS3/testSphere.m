67dc20ad7e41645ec5319d7a17a254360544dbf5
Leopold Clausse <clausseleopold@gmail.com>
2020-03-11 10:35:01 +0100

    Sphere ok

--- MS3/testSphere.m
+++ MS3/testSphere.m
@@ -2,25 +2,26 @@ clear;
 clc;
 
 [theta, phi, amplitude, phase] = importfileDiag("Diagramme.txt",1,1000000);
+% theta : 0 -> 180 par pas de 5
+% phi : 0 -> 360 par pas de 5
 
-%scatter();
+[phi, theta] = meshgrid(phi, theta);
 
-
-%[theta, phi] = meshgrid(theta, phi);
-
-X = amplitude .* sin(phi) .* cos(theta(1));
-Y = amplitude .* sin(phi) .* sin(theta(1));
+%{
+X = amplitude .* sin(phi) .* cos(theta);
+Y = amplitude .* sin(phi) .* sin(theta);
 Z = amplitude .* cos(phi);
+%}
+
+[x,y,z] = sph2cart(deg2rad(phi),deg2rad(theta),amplitude);
 
 
-
-scatter3(X,Y,Z,'.');
-
-
-%surf(X,Y,Z);
-
-%plot3(X,Y,Z);
-
-%Vq = interp3(X,Y,Z,V,Xq,Yq,Zq);
 
 %scatter3(X,Y,Z,'.');
+%warp(X,Y,Z,amplitude);
+%surf(phi,theta,zeros(size(phi)));
+surf(x,y,z);
+%scatter(x,z,'.');
+%plot3(x,y,z);
+%Vq = interp3(X,Y,Z,V,Xq,Yq,Zq);
+
