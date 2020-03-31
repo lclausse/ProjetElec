fab9645c7afba91e142564a3fdc1079d2e58993d
Leopold Clausse <clausseleopold@gmail.com>
2020-03-31 09:34:44 +0200

    Début

--- LABO 2/labo2.m
+++ LABO 2/labo2.m
@@ -1,3 +1,29 @@
 clear;
 clc;
 
+load("data_labo_reflexion.mat");
+
+subplot(2, 2, 1);
+plot(Reference);
+subplot(2, 2, 2);
+plot(Corr7);
+
+subplot(2, 2, 3);
+REF = fourier(Reference);
+plot(REF);
+subplot(2, 2, 4);
+CORR7 = fourier(Corr7);
+plot(CORR7);
+
+
+
+
+
+function [vec] = fourier(r)
+    L = length(r);
+    R = fft(r);
+    P2 = abs(R/L);
+    P1 = P2(1:L/2+1);
+    P1(2:end-1) = 2*P1(2:end-1);
+    vec = P1;
+end
