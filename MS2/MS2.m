cff0bbe8a7c1feea44f4183f95f3e59e100191c3
Leopold Clausse <clausseleopold@gmail.com>
2020-03-31 13:46:10 +0200

    Correlation ok

--- MS2/MS2.m
+++ MS2/MS2.m
@@ -32,24 +32,24 @@ ylabel('r_2(t)')
 R1 = fourier(r1);
 R2 = fourier(r2);
 
-f = Fs*(0:(L/2))/L;
+f = (-L/2:L/2-1)*(Fs/L); % zero-centered frequency range
 
 subplot(4,2,3);
 plot(f,R1) 
-title('Spectre unilatéral de r_1')
+title('Spectre de r_1')
 xlabel('f [Hz]')
 ylabel('|R_1(f)|')
 
 subplot(4,2,4);
 plot(f,R2) 
-title('Spectre unilatéral de r_2')
+title('Spectre de r_2')
 xlabel('f [Hz]')
 ylabel('|R_2(f)|')
 
 
 L_per = length(r1) + 2 * (length(r1) - 1);
-f_per = 3*Fs*(0:(L_per/2))/L_per;
-time_per = (Tsample/3) * (0:L_per-1);
+
+f_per = 3*(-L_per/2:L_per/2-1)*(Fs/L_per);
 % On ajoute des 0 entre les samples pour périodiser le spectre
 r1_per = zeros(1,L_per);
 r1_per(1:3:end) = r1;
@@ -61,18 +61,18 @@ R2_per = fourier(r2_per);
 
 subplot(4,2,5);
 plot(f_per,R1_per)
-title('Spectre unilatéral périodisé de r_1')
+title('Spectre périodisé de r_1')
 xlabel('f [Hz]')
 ylabel('|R1_{per}(f)|')
 
 subplot(4,2,6);
 plot(f_per,R2_per) 
-title('Spectre unilatéral périodisé de r_2')
+title('Spectre périodisé de r_2')
 xlabel('f [Hz]')
 ylabel('|R2_{per}(f)|')
 
 % On va mtn filtrer ce qui est en dessous de Fs = 3.2 GHz
-posFs = 2 * length(f_per) / 3;
+posFs = cast((2/3) * L_per, 'int32');
 
 R1_per_filtre = R1_per;
 R1_per_filtre(1:posFs) = 0;
@@ -82,51 +82,47 @@ R2_per_filtre(1:posFs) = 0;
 
 subplot(4,2,7);
 plot(f_per,R1_per_filtre)
-title('Spectre unilatéral périodisé filtré de r_1')
+title('Spectre périodisé filtré de r_1')
 xlabel('f [Hz]')
 ylabel('|R1_{filt}(f)|')
 
 subplot(4,2,8);
 plot(f_per,R2_per_filtre) 
-title('Spectre unilatéral périodisé filtré de r_2')
+title('Spectre périodisé filtré de r_2')
 xlabel('f [Hz]')
 ylabel('|R2_{filt}(f)|')
 
 
 % Corrélation entre signal r1 et r2 : 
+sample_corr = (-L_per/2:L_per/2-1); %(Tsample/3) * ;
 
-CORR = conj(R1_per_filtre) .* R2_per_filtre;
-corr = fourier_inverse(CORR);
+% En mode RF -> Remis bonne fréquence
+CORR_RF = conj(R1_per_filtre) .* R2_per_filtre;
+corr_RF = fourier_inverse(CORR_RF);
+
+% En mode bande de base
+CORR_BB = conj(R1) .* R2;
+corr_BB = fourier_inverse(CORR_BB);
 
 figure();
 
 subplot(2,1,1);
-plot(CORR);
+plot(CORR_RF);
 
 subplot(2,1,2);
-plot(corr);
-
-
-
-
+plot(sample_corr, corr_RF);
 
 
 function [vec] = fourier(r)
     L = length(r);
-    R = fft(r);
-    P2 = abs(R/L);
-    P1 = P2(1:L/2+1);
-    P1(2:end-1) = 2*P1(2:end-1);
-    vec = P1;
+    R = fftshift(fft(r));
+    vec = abs(R/L);
 end
 
 function [vec] = fourier_inverse(R)
-    L = length(R);
-    r = ifft(R);
-    P2 = abs(r/L);
-    P1 = P2(1:L/2+1);
-    P1(2:end-1) = 2*P1(2:end-1);
-    vec = P1;
+    %L = length(R);
+    r = fftshift(ifft(R));
+    vec = real(r);
 end
 
 
