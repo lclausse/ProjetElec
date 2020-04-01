6812fa0014ef9b3fc1ad69e8c0e3912975b962c6
Methylnaltrexone <francoiskollmann@gmail.com>
2020-04-01 10:28:35 +0200

    Update labo2.m
    ça marche pas encore, mais c'est ce que j'ai fait.
    tortank rajoute des 0 après le signal de référence, rajoute des 0 avant le signal copié et l'atténue.
    miaouss rajoute des 0 avant le signal de référence, autant que le décalage lu sur la corrélation, puis ajoute les 0 de la réflexion après le signal de référence, et ajoute les 2 paquets de 0 avant le signal de réflexion (refcopie) et l'atténue.
    puis ils font la corrélation. Ca marche pas ouf.

--- LABO 2/labo2.m
+++ LABO 2/labo2.m
@@ -4,20 +4,58 @@ clc;
 load("data_labo_reflexion.mat");
 
 subplot(2, 2, 1);
-plot(Reference);
-subplot(2, 2, 2);
-plot(Corr7);
+plot(abs(Corr7));
+decalagedebase = 102250;
+decalsupp = 115;
+attenuation = 0.6666;
+test1 = miaouss(decalagedebase,decalsupp,attenuation,Reference);
+subplot(2,2,2);
+plot(abs(test1));
+test2 = tortank(decalagedebase+1100,attenuation,Reference);
+subplot(2,2,3);
+plot(abs(test2));
+test3 = tortank(decalagedebase+1200,attenuation,Reference);
+subplot(2,2,4);
+plot(abs(test3));
 
 
 
 
+
+
+function [jeanphilippe] = tortank(sampledecal,attenuation,Ref)
+    decalage = zeros(1,sampledecal);
+    refcopie = [decalage,Ref]*attenuation;
+    nouvRef = [Ref,decalage];
+    FFTref = fourier(nouvRef);
+    FFTcopie = fourier(refcopie);
+    jeanphilippe = fourier_inverse(conj(FFTref).*FFTcopie);
+end
+
+
+function [vec] = miaouss(decalbase,decalreflet,attenuation,Ref)
+    decalage = zeros(1,decalbase);
+    decalageReflet = zeros(1,decalreflet);
+    nouvRef = [decalage,Ref,decalageReflet];
+    refCopie = [decalage,decalageReflet,Ref]*attenuation;
+    FFTref = fourier(nouvRef);
+    FFTcopie = fourier(refCopie);
+    vec = fourier_inverse(conj(FFTref).*FFTcopie);
+
+
+end
 
 
 function [vec] = fourier(r)
     L = length(r);
-    R = fft(r);
-    P2 = abs(R/L);
-    P1 = P2(1:L/2+1);
-    P1(2:end-1) = 2*P1(2:end-1);
-    vec = P1;
+    R = fftshift(fft(r));
+    vec = abs(R/L);
+end
+
+ 
+
+function [vec] = fourier_inverse(R)
+    %L = length(R);
+    r = fftshift(ifft(R));
+    vec = abs(r);
 end
